9286874e29b8992059ad542c4fe941506563d129
hleoc <informig@gmail.com>
2021-05-27 20:48:43 -0300

    Criado o servidor.

---
+++ src/shared/infra/http/server.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+const port = 3333;
+
+app.get("/", (_req, res) => res.send("Teddy Soluções"));
+
+app.listen(port, () => console.log(`Back-end started on localhost:${port}! 🚀`));
